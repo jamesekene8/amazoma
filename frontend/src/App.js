9300058bbef11b1002a2ddc01b3daf575cc2797f
James Uguna <jamesekene8@gmail.com>
2022-03-22 23:19:26 +0100

    Added routes to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
-import data from "./data";
+import { Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
@@ -7,25 +9,10 @@ function App() {
         <a href="/">Amazona</a>
       </header>
       <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div key={product.slug} className="product">
-              <a href={`/product/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  <strong>{product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
+        <Routes>
+          <Route path="/product/:slug" element={<ProductScreen />}></Route>
+          <Route path="/" element={<HomeScreen />}></Route>
+        </Routes>
       </main>
     </div>
   );
