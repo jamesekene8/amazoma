ac319fa9d23fb174eefce412ace60ef5c79a32d6
James Uguna <jamesekene8@gmail.com>
2022-03-28 16:24:12 +0100

    Created the userModel and seeded data to database

--- backend/routes/seedRoutes.js
+++ backend/routes/seedRoutes.js
@@ -1,13 +1,16 @@
 import express from "express";
 import data from "../data.js";
 import Product from "../models/productModel.js";
+import User from "../models/userModel.js";
 
 const seedRouter = express.Router();
 
 seedRouter.get("/", async (req, res) => {
   await Product.remove({});
   const createdProducts = await Product.insertMany(data.products);
-  res.send({ createdProducts });
+  await User.remove({});
+  const createdUser = await User.insertMany(data.users);
+  res.send({ createdUser, createdProducts });
 });
 
 export default seedRouter;
